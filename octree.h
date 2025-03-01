751727bc49d825f315c4a12414969fcdaa344f2c
attcs <att.csikos@gmail.com>
2025-03-01 23:47:04 +0100

    Add asserts

--- octree.h
+++ octree.h
@@ -369,6 +369,7 @@ namespace OrthoTree
 
       constexpr void push_back(const T& value)
       {
+        assert(m_size < N);
         m_stack[m_size] = value;
         ++m_size;
       }
@@ -383,6 +384,7 @@ namespace OrthoTree
 
       constexpr iterator insert(iterator whereIt, T const& val)
       {
+        assert(m_size < N);
         for (auto it = m_stack.begin() + m_size; it != whereIt; --it)
         {
           *it = std::move(*(it - 1));
@@ -394,6 +396,8 @@ namespace OrthoTree
 
       constexpr bool erase(iterator it)
       {
+        assert(m_size > 0);
+
         if (it < begin() || it >= end())
         {
           return false;
